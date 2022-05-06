759f85cb4599fa5aa972466f9330f8cc648b2272
Yannick Richter <git@y-richter.de>
2022-05-06 22:25:38 +0200

    noupx

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
-Test
-- Configurator
-- Actually configures
+- Split 1 and 2 axis mainclasses
+- Added CAN button source
+- Added analog CAN source
+- fast mode for PCF buttons
+- Moved CAN settings to common dialog
+- BISS-C encoder support
+- TMC graph follows system style
+- Windows darkmode support
+- Digital and analog readout
--- analogconf_ui.py
+++ analogconf_ui.py
@@ -1,8 +1,9 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtWidgets import QDialog
-from PyQt6.QtWidgets import QWidget,QGroupBox
+from PyQt6.QtWidgets import QWidget,QGroupBox,QProgressBar
 from PyQt6.QtWidgets import QMessageBox,QVBoxLayout,QCheckBox,QButtonGroup,QPushButton,QLabel,QSpinBox,QComboBox
 from PyQt6 import uic
+from PyQt6.QtCore import QTimer
 import main
 from helper import res_path,classlistToIds
 from optionsdialog import OptionsDialog,OptionsDialogGroupBox
@@ -34,17 +35,33 @@ class AnalogInputConf(OptionsDialogGroupBox,CommunicationHandler):
         self.buttonBoxLayout = QVBoxLayout()
         self.buttonBox.setLayout(self.buttonBoxLayout)
 
+        self.pgb_list=[]
+        self.axismask=0
+        self.prefix=0
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateTimer)
+
     def initUI(self):
         layout = QVBoxLayout()
         self.autorangeBox = QCheckBox("Autorange")
         layout.addWidget(self.autorangeBox)
         layout.addWidget(self.buttonBox)
         self.setLayout(layout)
+
+    # Tab is currently shown
+    def showEvent(self,event):
+        self.timer.start(300)
+
+    # Tab is hidden
+    def hideEvent(self,event):
+        self.timer.stop()
         
+    def updateTimer(self):
+        self.sendCommands("apin",["values"],self.prefix)
+
     def readValues(self):
         self.getValueAsync("apin","pins",self.createAinButtons,0,conversion=int)
         self.getValueAsync("apin","autocal",self.autorangeBox.setChecked,0,conversion=int)
-        
 
     def createAinButtons(self,axes):
         self.axes = axes
@@ -52,31 +69,59 @@ class AnalogInputConf(OptionsDialogGroupBox,CommunicationHandler):
         for i in range(self.buttonBoxLayout.count()):
             b = self.buttonBoxLayout.takeAt(0)
             self.buttonBoxLayout.removeItem(b)
-            b.widget().deleteLater()
+            b.widget().setParent(None)
+            #b.widget().deleteLater()
         for b in self.analogbtns.buttons():
             self.analogbtns.removeButton(b)
 
+        self.pgb_list.clear()
         # add buttons
         for i in range(axes):
             btn=QCheckBox(str(i+1),self)
+            pgb = QProgressBar(self)
+            pgb.setFixedHeight(10)
+            pgb.setRange(-32768, 32767)
+            pgb.setValue(-32768)
             self.analogbtns.addButton(btn,i)
             self.buttonBoxLayout.addWidget(btn)
+            self.buttonBoxLayout.addWidget(pgb)
+            self.pgb_list.append(pgb)
 
         def f(axismask):
+            self.axismask = axismask
             for i in range(self.axes):
                 self.analogbtns.button(i).setChecked(axismask & (1 << i))
         self.getValueAsync("apin","mask",f,0,conversion=int)
+
+    def valueCb(self, str):
+        val_list = str.split("\n")
+        #print(val_list)
+        j=0
+        for i in range(self.axes):
+            pgb = self.pgb_list[i]
+            if self.axismask & (1<<i):
+                pgb.setValue(int(val_list[j]))
+                j=j+1
+            else:
+                pgb.setValue(-32768)
 
     def apply(self):
         mask = 0
         for i in range(self.axes):
             if (self.analogbtns.button(i).isChecked()):
                 mask |= 1 << i
-
+        self.axismask = mask
         self.sendValue("apin","mask",mask)
         self.sendValue("apin","autocal",1 if self.autorangeBox.isChecked() else 0)
 
+    def onshown(self):
+        # pass
+        print("Open")
+        self.registerCallback("apin","values",self.valueCb,self.prefix,str)
+
     def onclose(self):
+        # pass
+        print("Close")
         self.removeCallbacks()
 
 
